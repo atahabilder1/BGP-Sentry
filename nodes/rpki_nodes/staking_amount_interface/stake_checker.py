9060bf69c8a004e44b36a0e82403a19ee0ba8c1e
atahabilder1 <tahabilderanik@gmail.com>
2025-07-29 22:51:49 -0400

    added two more interface

---
+++ nodes/rpki_nodes/staking_amount_interface/stake_checker.py
@@ -0,0 +1,1 @@
+# Staking Amount Checker\n\ndef get_stake_amount(address):\n    # TODO: implement stake amount lookup\n    return 3.0  # mock ETH value
---
+++ nodes/rpki_nodes/trust_score_interface/trust_score.py
@@ -0,0 +1,1 @@
+# Trust Score Checker\n\ndef get_trust_score(address):\n    # TODO: implement trust score lookup\n    return 100  # mock value
